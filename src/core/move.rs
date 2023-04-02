c8928e3510315f609765fb2b3f5ff76fabca6bc7
Marc Hernandez <marcsh@gmail.com>
2023-04-02 08:54:04 -0700

    x) Ooops, actually add it

---
+++ src/core/move.rs
@@ -0,0 +1,29 @@
+
+
+use super::*;
+
+struct MoveSys {
+
+}
+
+impl System for MoveSys {
+
+    fn start(&mut self) {
+
+    }
+
+    fn stop(&mut self) {
+
+    }
+}
+
+impl Ticks for MoveSys {
+    fn tick(&mut self) {
+
+    }
+
+    fn other(&mut self) {
+        
+    }
+
+}
